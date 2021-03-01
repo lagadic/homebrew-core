99ad31b0e7d938f0026151eb2e1aa9a83ed4f080
morusn <morusny12@outlook.com>
2021-03-01 18:23:20 +0100

    visp patch removed

--- Formula/visp.rb
+++ Formula/visp.rb
@@ -102,25 +102,3 @@ class Visp < Formula
   end
 end
 __END__
-diff --git a/modules/vision/src/key-point/vpKeyPoint.cpp b/modules/vision/src/key-point/vpKeyPoint.cpp
-index dd5cabf..23ed382 100644
---- a/modules/vision/src/key-point/vpKeyPoint.cpp
-+++ b/modules/vision/src/key-point/vpKeyPoint.cpp
-@@ -2269,7 +2269,7 @@ void vpKeyPoint::initDetector(const std::string &detectorName)
-
-   if (detectorNameTmp == "SIFT") {
- #ifdef VISP_HAVE_OPENCV_XFEATURES2D
--    cv::Ptr<cv::FeatureDetector> siftDetector = cv::xfeatures2d::SIFT::create();
-+    cv::Ptr<cv::FeatureDetector> siftDetector = cv::SIFT::create();
-     if (!usePyramid) {
-       m_detectors[detectorNameTmp] = siftDetector;
-     } else {
-@@ -2447,7 +2447,7 @@ void vpKeyPoint::initExtractor(const std::string &extractorName)
- #else
-   if (extractorName == "SIFT") {
- #ifdef VISP_HAVE_OPENCV_XFEATURES2D
--    m_extractors[extractorName] = cv::xfeatures2d::SIFT::create();
-+    m_extractors[extractorName] = cv::SIFT::create();
- #else
-     std::stringstream ss_msg;
-     ss_msg << "Fail to initialize the extractor: SIFT. OpenCV version  " << std::hex << VISP_HAVE_OPENCV_VERSION
